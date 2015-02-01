a2dafd4cf4afbc827f0ab0474784e42233ae92bb
cjblomqvist <carl@cjblomqvist.com>
2015-02-01 16:08:10 +0100

    Add packaging and rows collection

---
+++ packaging.js
@@ -0,0 +1,26 @@
+var _ = require('lodash');
+
+module.exports = function (derby) {
+  var name = 'Packaging';
+  var ns = 'packaging';
+  var Model = derby.Model;
+
+  if(Model.collectionExist(name, ns)) return;
+
+  var Packaging = Model.collection(name, ns);
+
+  Packaging.getAll = function(cb) {
+    var model = this.model;
+
+    model.subscribe(function (err) {
+      // Possibility to deal with error handling
+      if(cb) cb(err);
+    });
+  };
+
+  Packaging.toText = function(value) {
+    var packages = this.get();
+
+    return (_.findWhere(packages, {value: value}) || {}).name || '';
+  };
+};
---
+++ rows.js
@@ -0,0 +1,45 @@
+var _ = require('lodash');
+
+module.exports = function (derby) {
+  var name = 'Rows';
+  var ns = 'rows';
+  var Model = derby.Model;
+
+  if(Model.collectionExist(name, ns)) return;
+
+  var Rows = Model.collection(name, ns);
+
+  Rows.getFromMachine = function(step, machine, day, cb) {
+    var model = this.model;
+    var root = model.root;
+    var query = model.query(this.ns, {date: day, step: step, qty: {$gt: 0}});
+
+    query.subscribe(function (err) {
+      // Possibility to deal with error handling
+
+      if(cb) return cb(err, query);
+    });
+  };
+
+  Rows.getCurrentNextFromMachine = function (step, machine, day, time, cb) {
+    this.getFromMachine(step, machine, day, function (err, query) {
+      var rows = query.get();
+      var row = _getCurrentOrNext(time, rows);
+      var scoped = this.model.at(row.id);
+
+      if(cb) return cb(err, scoped);
+    }.bind(this));
+  };
+};
+
+function _getCurrentOrNext(time, list) {
+  list.sort(function (a, b) {
+    return a.temp.time - b.temp.time;
+  });
+
+  var i = _.findIndex(list, function (row) {
+        return (row.temp.time + row.temp.duration > time || row.temp.time > time);
+      });
+
+  return list[i];
+}
