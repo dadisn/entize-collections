7c8cd18ac03aa3f4b138dc70da0f6bd10735f474
Daði Snær Skúlason <dadisn@gmail.com>
2015-02-02 14:02:09 +0100

    Added createRun method to RunData

--- runData.js
+++ runData.js
@@ -6,4 +6,52 @@ module.exports = function (derby) {
   if(Model.collectionExist(name)) return;
 
   var RunData = Model.collection(name, ns);
+
+  RunData.createRun = function(step, machine, operators, artNr, artName, rowId, cb) {
+    var model = this.model;
+
+    // Allow for different ways to define operators
+    if(typeof operators === 'number') {
+      nrOfOperators = operators;
+      operators = [];
+    } else if(_.isArray(operators)) {
+      nrOfOperators = operators.length;
+    } else {
+      nrOfOperators = 1;
+      operators = [operators];
+    }
+
+    if(!this.model.root.hasOwnProperty('PreCheckRuns')) return cb(new Error('PreCheckRuns collection required for adding run data'));
+
+    this.model.root.PreCheckRuns.getRun(artNr, machine, function(err, preCheckRun) {
+      if(err) return cb(err);
+
+      var run = {
+          artNr: artNr
+        , artName: artName
+        , rowId: rowId
+        , step: step
+        , startTime: Date.now()
+        , finishTime: null
+        , stops: []
+        , activeDuration: null
+        , stopDuration: null
+        , totalDuration: null
+        , producedQty: null
+        , wasteQty: null
+        , machine: machine
+        , nrOfOperators: nrOfOperators
+        , operators: operators
+        , preChecks: []
+        , preCheckRun: preCheckRun.id
+        , preCheckRunPassed: false
+        , preCheckRunDuration: null
+      };
+
+      var id = model.add(run);
+
+      return cb(null, id);
+    });
+
+  };
 };
