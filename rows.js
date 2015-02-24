37de1b88f9b661622765d82d67c0f940a1011fea
John Barbero Unenge <john.barbero.unenge@gmail.com>
2015-02-24 14:43:34 +0100

    Avoid making queries from getters in Rows collection

--- rows.js
+++ rows.js
@@ -13,33 +13,25 @@ module.exports = function (derby) {
     var model = this.model;
     var root = model.root;
     var query = model.query(this.ns, {date: day, step: step, 'temp.machine': machine, qty: {$gt: 0}});
-
     query.subscribe(function (err) {
       // Possibility to deal with error handling
-
       if(cb) return cb(err, query);
     });
-  };
+  }
 
   Rows.getCurrentNextFromMachine = function (step, machine, day, time, cb) {
-    this.getFromMachine(step, machine, day, function (err, query) {
-      var rows = query.get();
-      var row = _getCurrentOrNext(time, rows);
-      var scoped = (row && row.id) ? this.model.at(row.id) : undefined;
+    var model = this.model;
+    model.fn('matchingCriterias', function(row) {
+      return row.step === step && row.temp.machine === machine && row.date === day;
+    });
+    model.fn('timeSort', function(a, b) {
+      return a.temp.time - b.temp.time;
+    });
 
-      if(cb) return cb(err, scoped);
-    }.bind(this));
-  };
-};
-
-function _getCurrentOrNext(time, list) {
-  list.sort(function (a, b) {
-    return a.temp.time - b.temp.time;
-  });
-
-  var i = _.findIndex(list, function (row) {
-    return (row.temp.time + row.temp.duration > time || row.temp.time > time);
-  });
-
-  return list[i];
+    var filter = this.filter('matchingCriterias').sort('timeSort');
+    var row = _.find(filter.get(), function(row) {
+      return (row.temp.time + row.temp.duration > time || row.temp.time > time);
+    });
+    return cb(undefined, row);
+  }
 }
