31a34b3bb791194644945644cd2f9635ff617849
Jesper Persson <jespert.persson@gmail.com>
2015-02-24 16:22:04 +0100

    Add sift as dependency and use it to create filterFns

--- rows.js
+++ rows.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var sift = require('sift');
 
 module.exports = function (derby) {
   var name = 'Rows';
@@ -12,7 +13,9 @@ module.exports = function (derby) {
   Rows.getFromMachine = function(step, machine, day, cb) {
     var model = this.model;
     var root = model.root;
-    var query = model.query(this.ns, {date: day, step: step, 'temp.machine': machine, qty: {$gt: 0}});
+    var conditions = {date: day, step: step, 'temp.machine': machine, qty: {$gt: 0}};
+    this.filterFn = sift(conditions);
+    var query = model.query(this.ns, conditions);
     query.subscribe(function (err) {
       // Possibility to deal with error handling
       if(cb) return cb(err, query);
@@ -21,14 +24,12 @@ module.exports = function (derby) {
 
   Rows.getCurrentNextFromMachine = function (step, machine, day, time, cb) {
     var model = this.model;
-    model.fn('matchingCriterias', function(row) {
-      return row.step === step && row.temp.machine === machine && row.date === day;
-    });
+
     model.fn('timeSort', function(a, b) {
       return a.temp.time - b.temp.time;
     });
 
-    var filter = this.filter('matchingCriterias').sort('timeSort');
+    var filter = this.filter(this.filterFn).sort('timeSort');
     var row = _.find(filter.get(), function(row) {
       return (row.temp.time + row.temp.duration > time || row.temp.time > time);
     });
